587d67ce862a7acc0c14182d560314d3aacc2943
Jeremiah Larry King Ungsod <67980301+Jeremiah019@users.noreply.github.com>
2023-04-27 12:49:24 +0800

    Update login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <style>
+    .divider:after,
+    .divider:before 
+    {
+        content: "";
+        flex: 1;
+        height: 1px;
+        background: #eee;
+    }
+    </style>
 </head>
 <body>
     <h1>Login</h1>
@@ -14,23 +30,56 @@
             </ul>
         </div>
     @endif
-    <form action="{{ url('/login') }}" method="POST">
+    <section class="vh-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex align-items-center justify-content-center h-100">
+      <div class="col-md-8 col-lg-7 col-xl-6">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+          class="img-fluid" alt="Phone image">
+      </div>
+      <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+        <form action="{{ url('/login') }}" method="POST">
         {!! csrf_field() !!}
-        <div>
-            Email
-            <input type="email" name="email" value="{{ old('email') }}">
-        </div>
-        <div>
-            Password
-            <input type="password" name="password">
-        </div>
-        <div>
-            <input type="checkbox" name="remember"> Remember Me
-        </div>
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
-    <p>Don't have an account? <a href="{{ url('/register') }}">Sign up</a></p>
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+            <input type="email" id="form1Example13" class="form-control form-control-lg" value="{{ old('email') }}">
+            <label class="form-label" for="form1Example13">Email address</label>
+          </div>
+
+          <!-- Password input -->
+          <div class="form-outline mb-4">
+            <input type="password" id="form1Example23" class="form-control form-control-lg" />
+            <label class="form-label" for="form1Example23">Password</label>
+          </div>
+
+          <div class="d-flex justify-content-around align-items-center mb-4">
+            <!-- Checkbox -->
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+              <label class="form-check-label" for="form1Example3"> Remember me </label>
+            </div>
+            <a href="#!">Forgot password?</a>
+          </div>
+
+          <!-- Submit button -->
+          <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+
+          <div class="divider d-flex align-items-center my-4">
+            <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+          </div>
+
+          <a class="btn btn-primary btn-lg btn-block" style="background-color: #3b5998" href="{{ url('/register') }}"
+            role="button">
+            <i class="fab fa-facebook-f me-2"></i>Sign Up
+          </a>
+          <!-- <a class="btn btn-primary btn-lg btn-block" style="background-color: #55acee" href="#!"
+            role="button">
+            <i class="fab fa-twitter me-2"></i>Continue with Twitter</a> -->
+        </form>
+        <!-- <p>Don't have an account? <a href="{{ url('/register') }}">Sign up</a></p> -->
+      </div>
+    </div>
+  </div>
+</section>
 </body>
 </html>
